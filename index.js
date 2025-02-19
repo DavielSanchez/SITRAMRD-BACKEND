94b542984cff7a83fc9ff120278493bde001a229
gustavo <gustavo_angel27@hotmail.com>
2025-02-19 18:43:12 -0400

    feat(api): Se implemento la funcionalibidad de un administrador para manejar los roles de los demas usuarios

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { mongoConnection } = require('./DB')
 dotenv.config();
 const app = express();
 const Ruta = require('./Endpoints/Ruta')
+const Usuarios = require('./Endpoints/Usuario')
 
 const Authentication = require('./Endpoints/Authentication')
 
@@ -31,6 +32,7 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs))
 
 app.use('/auth', Authentication)
 app.use('/ruta', Ruta)
+app.use('/usuario', Usuarios)
 
 /**
  * @openapi
